5a151341aeaebf6026a23d6561d2dce51986dc03
Navjot Dhiman <92823851+Navi003@users.noreply.github.com>
2024-10-26 22:15:15 +0200

    Intial comit

---
+++ src/components/CoinRolls.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+export default function CoinRolls() {
+  return (
+    <>
+      <div className="flex justify-between mx-5">
+        <h2 className=" text-center">Rollen</h2>
+        <div>Collaps</div>
+      </div>
+      <div className="bg-purple-400 p-4 flex justify-center flex-wrap gap-3">
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+        <div className="space-x-2">
+          <input
+            type="number"
+            className="w-20 p-4 rounded-md"
+            placeholder="2€"
+          />
+        </div>
+
+        <div>3265€</div>
+      </div>
+    </>
+  );
+}
